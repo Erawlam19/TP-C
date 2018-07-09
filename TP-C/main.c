065c489740cf8c2d8f570b463aef2e613334d535
RaphaelDG <Raphael.dulong@gmail.com>
2018-07-09 12:47:07 +0200

    clean

--- TP-C/main.c
+++ TP-C/main.c
@@ -6,7 +6,7 @@ typedef struct
 {
     char id[150];
     char nomPrenom[150];
-    char dateNaissance[20];
+    char dateNaissance[150];
     char sexe;
     int nbEnfants;
     char enfants[260*4]; //record d'enfants pour un humain
@@ -19,11 +19,22 @@ int charcount(char * filename);
 
 int main()
 {
-    Personne *lesPersonnes;
-    lesPersonnes = ChargerPersonnes;
+    Personne lesPersonnes[10];
 
-    printf("%s\n", lesPersonnes[0].id);
-    return 0;
+    Personne Personne1;
+    strcpy(Personne1.id , "P001");
+    strcpy(Personne1.dateNaissance , "21/06/1996");
+    Personne1.nbEnfants = 3;
+    Personne1.sexe = 'M';
+    lesPersonnes[0] = Personne1;
+
+    Personne Personne2;
+    strcpy(Personne2.dateNaissance , "P003");
+    strcpy(Personne2.dateNaissance , "21/06/1996");
+    Personne2.nbEnfants = 3;
+    Personne2.sexe = 'F';
+    lesPersonnes[1] = Personne1;
+
 }
 
 Personne * ChargerPersonnes()
@@ -105,3 +116,126 @@ int charcount(char * filename) //retourne le nb de caractere de la plus grande l
     fclose(fin);
     return count;
 }
+
+void Menu()
+{
+    int choix;
+    printf("\n*****************************************************************************\n");
+    printf("Que souhaitez vous faire ?\n\n");
+    printf(" - Ajouter une personne : tapez 1 \n");
+    printf(" - Afficher larbre complet dune personne : tapez 2\n");
+    printf(" - Obtenir des statistiques : tapez 3\n");
+    printf(" - Rechercher des parents : tapez 4\n");
+    printf("Cest a vous : ");
+    scanf("%d", &choix);
+
+    switch (choix)
+    {
+        case 1:
+            AjoutPersonne();
+            break;
+        case 2:
+            AfficherArbre();
+            break;
+        case 3:
+            nbrPersSansEnfants();
+            nbrMoyeEnf();
+            nbrHetF();
+            PersJeune();
+            break;
+        case 4:
+            RechercheParents();
+            break;
+        default:
+            printf("!! Votre saisie est incorrecte, veuillez reessayer !!");
+            Menu();
+          break;
+    }
+}
+
+
+
+void nbrPersSansEnfants(Personne * tableau)
+{
+    tableau[i].enfants
+    int i, compteur, taille;
+    taille = sizeof(Personne);
+
+    for(i = 0; i < taille; i++){
+        if(UnePersonne[i]. == 0)
+        {
+            compteur =+ 1;
+            printf("Il y a %d personnes, sans enfants", compteur);
+        }
+    }
+}
+
+void nbrMoyeEnf()
+{
+    int i, compteur, nbrdenfants, moyenne;
+    taille = sizeof(Personne);
+
+
+    for(i = 0; i < taille; i++){
+        if(UnePersonne[i].enfants != 0)
+        {
+            compteur =+ 1;
+            nbrdenfants = nbrdenfants + personne[i].enfants;
+            moyenne = nbrdenfants/compteur;
+
+            printf("Il y a %d personnes, sans enfants", compteur);
+        }
+        //printf("%d.%s\n", i, sp[i].name);
+    }
+}
+
+
+void nbrHetF()
+{
+    int i, compteurH, compteurF, taille;
+    taille = sizeof(Personne);
+
+
+    for(i = 0; i < taille; i++){
+        if(UnePersonne[i].sexe == 'H')
+        {
+            compteurH =+ 1;
+        }
+        if(personne[i].sexe == 'F')
+        {
+            compteurF =+ 1;
+        }
+
+        printf("Il y a %d homme(s) au total", compteurH);
+        printf("Il y a %d femme(s) au total", compteurF);
+
+    }
+}
+
+void PersJeune()
+{
+    int i, compteur;
+    taille = sizeof(Personne);
+
+
+    for(i = 0; i < taille; i++){
+        char str[] = UnePersonne[i].dateNaissance;
+        char *token = strtok(str, "/");
+
+        while (token != NULL)
+        {
+            printf("%s\n", token);
+            token = strtok(NULL, "/");
+        }
+
+        if()
+    }
+}
+
+/*
+Nombre de personnes sans enfants - OK
+ Nombre moyen d’enfants par personne - OK
+ Quel est le nom et prénom de la personne la plus jeune ?
+ Quel est le nom et prénom de la personne la plus agée ?
+ Nombre d’hommes et de femmes - OK
+*/
